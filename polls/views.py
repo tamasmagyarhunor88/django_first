8cedf9ca542efd94ff9fe506fa8066bf13216ebe
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2021-10-25 21:22:44 +0100

    feat(tutorial03-views) - Add views to view question, results of a question and vote on a question

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at them polls index.")
+
 
 def hunor(request):
     return HttpResponse("Magyar-Hunor Tamas £1000 per day, Porsche incoming")
 
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)

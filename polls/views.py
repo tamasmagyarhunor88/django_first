2b5670fbb73c5557f62c804c5222914d80c33867
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2021-10-25 22:17:09 +0100

    feat(tutorial03-templates) - Update detail view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from .models import Question
 # Create your views here.
@@ -16,7 +17,11 @@ def hunor(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
